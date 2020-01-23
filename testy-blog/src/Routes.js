a25cba35e00f59a8918027fc21fbb1a3be5757f8
Mirbek Muratov <mirbekmuratov@MacBook-Air-Mirbek.local>
2020-01-23 14:31:42 +0600

    first-project

---
+++ testy-blog/src/Routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import React, { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+
+
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Routes;
---
+++ testy-blog/src/component/HomePage/Feed/Feed.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import Directory from '../Directory/Directory'
+import HelloPage from '../HelloPage/HelloPage'
+import List from '../List/List'
+import Details from '../Details/Details'
+
+
+class Feed extends React.Component {
+  render() {
+    return (
+      <div>
+        <Directory 
+        
+        />
+        <HelloPage 
+
+        />
+        <List
+        
+        />
+        <Details 
+        
+        />
+      </div>
+    );
+  }
+}
+
+export default Feed;
