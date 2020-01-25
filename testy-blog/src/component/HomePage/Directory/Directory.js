146256d1fd734e68e67c64fce7660bb04a8168d7
daniyar-AKM <daniiarakmatov@gmail.com>
2020-01-25 11:18:02 +0600

    Directory changed

--- testy-blog/src/component/HomePage/Directory/Directory.js
+++ testy-blog/src/component/HomePage/Directory/Directory.js
@@ -1,10 +1,7 @@
 import React from "react";
 import axios from "axios";
 import './Directory.css';
-import breakfast from './breakfast.png';
-import cakes from './cakes.png';
-import soups from './soups.png';
-import pizza from './pizza.png';
+
 
 class Directory extends React.Component {
   state = {
@@ -32,12 +29,15 @@ class Directory extends React.Component {
       <div>
           <div className="category">
             {this.state.categoryList.map(item => {
-            return <button key={item.id} onClick={() => this.handleClickCategory(item.name)} >{item.name} </button>
+            return (
+              <div>
+                <a href="#" key={item.id} onClick={() => this.handleClickCategory(item.name)} > 
+                  <img src={item.image}></img> 
+                  <div className="Text-dir">{item.name}</div> 
+                </a>
+              </div>
+            )
             })}
-            <img src={breakfast}></img>
-            <img src={cakes}></img>
-            <img src={soups}></img>
-            <img src={pizza}></img>
           </div>
       </div>
     );
--- testy-blog/src/component/HomePage/NavBar/NavBar.js
+++ testy-blog/src/component/HomePage/NavBar/NavBar.js
@@ -7,10 +7,10 @@ class NavBar extends React.Component {
       <div className="nav-bar">
         <div className="header">
           <div className="logo">
-            <h1>Tasty plan</h1>
+            <a href="/"><h1>Tasty plan</h1></a>
           </div>
           <ul className="menu">
-            <li className="menu__item">ДОМОЙ</li>
+            <a href="/"><li className="menu__item">ДОМОЙ</li></a>
             <li className="menu__item">ДОБАВИТЬ РЕЦЕПТ</li>
             <li className="menu__item">О НАС</li>
           </ul>
